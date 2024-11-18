9d323266c4c6da0f43e8798253c09e02bcce54f1
lyx12344321 <3469686919@qq.com>
2024-11-18 21:01:25 +0800

    初始化项目

---
+++ src/main/java/io/github/weightrack/WeighTrackApplication.java
@@ -0,0 +1,13 @@
+package io.github.weightrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeighTrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeighTrackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/weightrack/WeighTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.weightrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeighTrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
