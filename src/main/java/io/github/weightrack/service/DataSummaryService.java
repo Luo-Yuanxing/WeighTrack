4dcb5271b99fca73cb54c859f6a89d1c1a6a8811
lyx12344321 <3469686919@qq.com>
2024-12-15 15:54:33 +0800

    fix bug

--- src/main/java/io/github/weightrack/service/DataSummaryService.java
+++ src/main/java/io/github/weightrack/service/DataSummaryService.java
@@ -1,20 +1,14 @@
 package io.github.weightrack.service;
 
-import com.alibaba.fastjson2.JSON;
 import io.github.weightrack.mapper.CoalTypeMapper;
 import io.github.weightrack.mapper.DataSummaryMapper;
 import io.github.weightrack.module.CoalType;
-import io.github.weightrack.module.PoundBillModel;
-import io.github.weightrack.module.SummaryTable;
-import io.github.weightrack.utils.DateUtil;
 import org.intellij.lang.annotations.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-import java.time.chrono.JapaneseDate;
-import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class DataSummaryService {
@@ -26,9 +20,8 @@ public class DataSummaryService {
     DataSummaryMapper dataSummaryMapper;
 
     public List<Map<String, Object>> getCountGroupByCoalType(String IOType, String start, String end) {
-        List<Map<String, Object>> countByCoalTypes = dataSummaryMapper.getCountGroupByCoalType(IOType, start, end);
-        System.out.println(countByCoalTypes);
-        return countByCoalTypes;
+        //        System.out.println(countByCoalTypes);
+        return dataSummaryMapper.getCountGroupByCoalType(IOType, start, end);
     }
 
     public List<Map<String, Object>> getCountGroupByDate(@Pattern("^([10])$") String IOType, String start, String end) {
