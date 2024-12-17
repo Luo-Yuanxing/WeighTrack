3ec3ac850e90fcd04e66d09649f20e13135ed6c3
lyx12344321 <3469686919@qq.com>
2024-12-17 16:54:48 +0800

    优化打印时间显示及Excel处理逻辑

--- src/main/java/io/github/weightrack/utils/ExcelUtil.java
+++ src/main/java/io/github/weightrack/utils/ExcelUtil.java
@@ -4,7 +4,6 @@ import io.github.weightrack.module.PoundBillModel;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -12,13 +11,9 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.sql.Date;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.ZoneOffset;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
 
 @Slf4j
 public class ExcelUtil {
@@ -27,7 +22,7 @@ public class ExcelUtil {
         int modifyLength = 0;
         try (FileInputStream fileInputStream = new FileInputStream(file);
              Workbook workbook = new XSSFWorkbook(fileInputStream);
-             FileOutputStream fileOutputStream = new FileOutputStream(file);) {
+             FileOutputStream fileOutputStream = new FileOutputStream(file)) {
 
             Sheet sheet;
             if (IOType.equals("1")) {
@@ -66,9 +61,7 @@ public class ExcelUtil {
             // poundBillModels排序
             Arrays.sort(poundBillModels, Comparator.comparingLong(poundBillModel ->
                     poundBillModel.getCreatTime().toInstant(ZoneOffset.UTC).getEpochSecond()));
-            List<PoundBillModel> poundBillModelList = new ArrayList<>(Arrays.asList(poundBillModels));
-            poundBillModelList = poundBillModelList.reversed();
-            for (PoundBillModel poundBillModel : poundBillModelList) {
+            for (PoundBillModel poundBillModel : poundBillModels) {
                 modifyLength++;
                 Row currentRow = sheet.getRow(i);
                 if (currentRow == null) {
@@ -118,7 +111,7 @@ public class ExcelUtil {
 
                     // 设置打印时间
                     Cell printTimeCell = currentRow.createCell(9, CellType.NUMERIC);
-                    printTimeCell.setCellValue(poundBillModel.getPrintTime());
+                    printTimeCell.setCellValue(poundBillModel.getCreatTime());
 
                     // 设置发货单位
                     Cell deliveryUnitCell = currentRow.createCell(10, CellType.STRING);
@@ -135,7 +128,7 @@ public class ExcelUtil {
                 } else {
                     // 设置打印时间
                     Cell printTimeCell = currentRow.createCell(7, CellType.NUMERIC);
-                    printTimeCell.setCellValue(poundBillModel.getPrintTime());
+                    printTimeCell.setCellValue(poundBillModel.getCreatTime());
 
                     // 设置发货单位
                     Cell deliveryUnitCell = currentRow.createCell(8, CellType.STRING);
@@ -161,207 +154,5 @@ public class ExcelUtil {
             throw new RuntimeException(e);
         }
         return modifyLength;
-    }
-
-    public static List<PoundBillModel> insertPoundBillByExcel() throws Exception {
-        //  NUMERIC	 STRING	 STRING	 STRING	 NUMERIC(毛重)	 NUMERIC	 FORMULA	 NUMERIC	 FORMULA	 NUMERIC	 STRING	 STRING	 STRING	 BLANK
-        List<PoundBillModel> poundBillModels = new ArrayList<>();
-
-        FileInputStream fileIn = new FileInputStream("D:\\零时文件\\WeChat Files\\luoyaping2012\\FileStorage\\File\\2024-12\\每日过磅明细-1.xlsx");
-        Workbook workbook = new XSSFWorkbook(fileIn);
-        Sheet sheet = workbook.getSheet("入库明细");
-        int length = sheet.getLastRowNum();
-        System.out.println("sheet长度：" + length);
-        for (int i = 3; i <= sheet.getLastRowNum(); i++) {
-
-            Row row = sheet.getRow(i);
-            PoundBillModel poundBillModel = new PoundBillModel();
-            if (row.getCell(0) == null) {
-                continue;
-            }
-            if (row.getCell(0).getCellType().equals(CellType.NUMERIC)) {
-                int excelDate = (int) row.getCell(0).getNumericCellValue();
-                LocalDate baseDate = LocalDate.of(1900, 1, 1);
-                long daysToAdd = (long) excelDate - 2;
-                LocalDate date = baseDate.plusDays(daysToAdd);
-
-                double printTime;
-                try {
-                    printTime = row.getCell(9).getNumericCellValue();
-                } catch (Exception e) {
-                    continue;
-                }
-                LocalDateTime localDateTime = getLocalDateTime(printTime, date);
-                poundBillModel.setCreatTime(localDateTime);
-                poundBillModel.setPrintTime(localDateTime);
-                poundBillModel.setModifyTime(localDateTime);
-
-                String poundID;
-                Cell poundIDCell = row.getCell(1);
-                if (poundIDCell.getCellType().equals(CellType.NUMERIC)) {
-                    System.out.println("poundID:" + poundIDCell.getNumericCellValue());
-                    poundID = "R" + (int) poundIDCell.getNumericCellValue();
-                } else if (poundIDCell.getCellType().equals(CellType.STRING)) {
-                    poundID = poundIDCell.getStringCellValue();
-                } else {
-                    poundID = "";
-                }
-                poundBillModel.setPoundID(poundID);
-
-                poundBillModel.setCoalType(row.getCell(2).getStringCellValue());
-                poundBillModel.setPlateNumber(row.getCell(3).getStringCellValue());
-
-                boolean grossFlag = false;
-                boolean tareFlag = false;
-                boolean primaryFlag = false;
-
-                try {
-                    poundBillModel.setGrossWeight((float) row.getCell(4).getNumericCellValue());
-                    grossFlag = true;
-                } catch (Exception ignored) {
-                }
-                try {
-                    poundBillModel.setTareWeight((float) row.getCell(5).getNumericCellValue());
-                    tareFlag = true;
-                } catch (Exception ignored) {
-                }
-                try {
-                    poundBillModel.setPrimaryWeight((float) row.getCell(7).getNumericCellValue());
-                    primaryFlag = true;
-                } catch (Exception ignored) {
-                }
-
-                if (grossFlag && tareFlag) {
-                    poundBillModel.setNetWeight(poundBillModel.getGrossWeight() - poundBillModel.getTareWeight());
-
-                    if (primaryFlag) {
-                        poundBillModel.setProfitLossWeight(poundBillModel.getNetWeight() - poundBillModel.getPrimaryWeight());
-                    }
-                }
-                if (row.getCell(10) != null) {
-                    poundBillModel.setOutputUnit(row.getCell(10).getStringCellValue());
-                }
-                if (row.getCell(11) != null) {
-                    poundBillModel.setInputUnit(row.getCell(11).getStringCellValue());
-                }
-                if (row.getCell(12) != null) {
-                    poundBillModel.setWeigher(row.getCell(12).getStringCellValue());
-                }
-
-                poundBillModel.setIOType("1");
-                poundBillModel.setPrinted(true);
-            }
-            System.out.println(poundBillModel);
-            poundBillModels.add(poundBillModel);
-        }
-
-        workbook.close();
-        fileIn.close();
-        return poundBillModels;
-    }
-
-    public static List<PoundBillModel> insertPoundBillByExcel_OUT() throws Exception {
-        //  NUMERIC	 STRING	 STRING	 STRING	 NUMERIC(毛重)	 NUMERIC	 FORMULA	 NUMERIC	 FORMULA	 NUMERIC	 STRING	 STRING	 STRING	 BLANK
-        List<PoundBillModel> poundBillModels = new ArrayList<>();
-
-        FileInputStream fileIn = new FileInputStream("D:\\零时文件\\WeChat Files\\luoyaping2012\\FileStorage\\File\\2024-12\\每日过磅明细-1.xlsx");
-        Workbook workbook = new XSSFWorkbook(fileIn);
-        Sheet sheet = workbook.getSheet("出库明细");
-        int length = sheet.getLastRowNum();
-        System.out.println("sheet长度：" + length);
-        for (int i = 2; i <= sheet.getLastRowNum(); i++) {
-
-            Row row = sheet.getRow(i);
-            PoundBillModel poundBillModel = new PoundBillModel();
-            if (row.getCell(0) == null) {
-                continue;
-            }
-            if (row.getCell(0).getCellType().equals(CellType.NUMERIC)) {
-                int excelDate = (int) row.getCell(0).getNumericCellValue();
-                LocalDate baseDate = LocalDate.of(1900, 1, 1);
-                long daysToAdd = (long) excelDate - 2;
-                LocalDate date = baseDate.plusDays(daysToAdd);
-
-                double printTime;
-                try {
-                    printTime = row.getCell(7).getNumericCellValue();
-                } catch (Exception e) {
-                    continue;
-                }
-                LocalDateTime localDateTime = getLocalDateTime(printTime, date);
-                poundBillModel.setCreatTime(localDateTime);
-                poundBillModel.setPrintTime(localDateTime);
-                poundBillModel.setModifyTime(localDateTime);
-
-                String poundID;
-                Cell poundIDCell = row.getCell(1);
-                if (poundIDCell.getCellType().equals(CellType.NUMERIC)) {
-                    System.out.println("poundID:" + poundIDCell.getNumericCellValue());
-                    poundID = "C" + (int) poundIDCell.getNumericCellValue();
-                } else if (poundIDCell.getCellType().equals(CellType.STRING)) {
-                    poundID = poundIDCell.getStringCellValue();
-                } else {
-                    poundID = "";
-                }
-                poundBillModel.setPoundID(poundID);
-
-                poundBillModel.setCoalType(row.getCell(2).getStringCellValue());
-                poundBillModel.setPlateNumber(row.getCell(3).getStringCellValue());
-
-                boolean grossFlag = false;
-                boolean tareFlag = false;
-                boolean primaryFlag = false;
-
-                try {
-                    poundBillModel.setGrossWeight((float) row.getCell(4).getNumericCellValue());
-                    grossFlag = true;
-                } catch (Exception e) {
-
-                }
-                try {
-                    poundBillModel.setTareWeight((float) row.getCell(5).getNumericCellValue());
-                    tareFlag = true;
-                } catch (Exception e) {
-
-                }
-
-                if (grossFlag && tareFlag) {
-                    poundBillModel.setNetWeight(poundBillModel.getGrossWeight() - poundBillModel.getTareWeight());
-                }
-                if (row.getCell(8) != null) {
-                    poundBillModel.setOutputUnit(row.getCell(8).getStringCellValue());
-                }
-                if (row.getCell(9) != null) {
-                    poundBillModel.setInputUnit(row.getCell(9).getStringCellValue());
-                }
-                if (row.getCell(10) != null) {
-                    poundBillModel.setWeigher(row.getCell(10).getStringCellValue());
-                }
-
-                poundBillModel.setIOType("0");
-                poundBillModel.setPrinted(true);
-            }
-            System.out.println(poundBillModel);
-            poundBillModels.add(poundBillModel);
-        }
-
-        workbook.close();
-        fileIn.close();
-        return poundBillModels;
-    }
-
-    @NotNull
-    private static LocalDateTime getLocalDateTime(double printTime, LocalDate date) {
-        double timePart = printTime - Math.floor(printTime);  // 获取小数部分
-
-        // 将小数部分转换为 LocalTime
-        int hours = (int) (timePart * 24); // 计算小时
-        int minutes = (int) ((timePart * 24 * 60) % 60); // 计算分钟
-        int seconds = (int) ((timePart * 24 * 3600) % 60); // 计算秒
-
-        // 创建 LocalTime 实例
-        LocalTime localTime = LocalTime.of(hours, minutes, seconds);
-
-        return LocalDateTime.of(date, localTime);
     }
 }
